d0372937c2f2ebf9e77ea6e3fe4aa9f905f1438d
scole02 <samcole107@gmail.com>
2024-04-02 15:31:08 -0700

    Add basic visualization

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
 import streamlit as st
+import pandas as pd
+import seaborn as sns
 
 st.set_page_config(
-    page_title="Penguins Explorer", 
+    page_title="Sam's Penguins Explorer", 
     page_icon="🐧", 
     layout="centered",
 )
 
 st.title("🐧 Penguins Explorer")
+
+df = pd.read_csv('https://raw.githubusercontent.com/mcnakhaee/palmerpenguins/master/palmerpenguins/data/penguins.csv')
+
+st.write(df)
+
+# Drop rows with missing values to ensure the plot works correctly
+df.dropna(inplace=True)
+
+# Create a pair plot
+pair_plot = sns.pairplot(df, hue='species', markers=["o", "s", "D"], corner=True)
+
+# Set the title of the pair plot
+pair_plot.fig.suptitle("Penguins Measurement Pair Plot", y=1.02)
+
+# Display the plot in the Streamlit app
+st.pyplot(pair_plot.fig)
